a966d041fce54b2de4186c772aa0e482c0c66a8f
Carlos <cguzmancamus@gmail.com>
2016-04-25 01:08:19 -0300

    Added NullObject Pattern

---
+++ NullObject/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NullObject
+{
+    public interface ICellPhone
+    {
+        void PrintInfo();
+    }
+
+    public class Nokia : ICellPhone
+    {
+        public void PrintInfo()
+        {
+            Console.WriteLine("Nokia!");
+        }
+    }
+
+    public class Samsung : ICellPhone
+    {
+        public void PrintInfo()
+        {
+            Console.WriteLine("Samsung!");
+        }
+    }
+
+    public class Iphone : ICellPhone
+    {
+        public void PrintInfo()
+        {
+            Console.WriteLine("IPhone!");
+        }
+    }
+
+    public class NullPhone : ICellPhone
+    {
+        public void PrintInfo()
+        {
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<ICellPhone> cellPhoneList = new List<ICellPhone>();
+            cellPhoneList.Add(new Iphone());
+            cellPhoneList.Add(new Samsung());
+            cellPhoneList.Add(new NullPhone());
+            cellPhoneList.Add(new Samsung());
+            cellPhoneList.Add(new Nokia());
+            cellPhoneList.Add(new Iphone());
+
+            foreach (var cell in cellPhoneList)
+            {
+                cell.PrintInfo();
+            }
+        }
+    }
+}
